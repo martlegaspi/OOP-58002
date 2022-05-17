5692c7ef37282e4212144b59a8c69680a352be35
martlegaspi <98940218+martlegaspi@users.noreply.github.com>
2022-05-17 15:36:42 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 import pyodbc
 try:
-    connection = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\MJ\Download(D)\Database1.accdb;')
+    connection = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\sayom\OneDrive\Documents\Database3.accdb;')
     print("Database is connected")
 
     Address = 'Navotas'
