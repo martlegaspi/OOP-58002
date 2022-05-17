040bc344e65ee40c0e6e36512d20fb83d11d31f8
martlegaspi <98940218+martlegaspi@users.noreply.github.com>
2022-05-17 15:32:26 +0800

    Update connecting.py

--- connecting.py
+++ connecting.py
@@ -1,7 +1,7 @@
 import pyodbc
 
 try:
-    connection = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\MJ\Download(D)\Database1.accdb;')
+    connection = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\sayom\OneDrive\Documents\Database3.accdb;')
     print("Database is connected")
 
 
