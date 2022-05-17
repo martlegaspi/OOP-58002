5061cdda6e6cd6312d691aac580a564c5b73f001
martlegaspi <98940218+martlegaspi@users.noreply.github.com>
2022-05-17 15:32:59 +0800

    Update deletion.py

--- deletion.py
+++ deletion.py
@@ -1,7 +1,7 @@
 import pyodbc
 
 try:
-    connection = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\MJ\Download(D)\Database1.accdb;')
+    connection = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\sayom\OneDrive\Documents\Database3.accdb;')
     
     user_id = 6
     record = connection.cursor()
